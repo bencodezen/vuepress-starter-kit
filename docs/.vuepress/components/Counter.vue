175c5e083577d31d8bb71dd36d95312098790f49
Ben Hong <bencodezen@gmail.com>
2019-04-09 09:29:42 -0400

    :sparkles: Add styling to Counter component example

--- docs/.vuepress/components/Counter.vue
+++ docs/.vuepress/components/Counter.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <p>{{ count }}</p>
-        <button @click="count++">Increment</button>
+    <div class="counter">
+        <p class="counter-count">{{ count }}</p>
+        <button class="counter-button" @click="count++">Add</button>
     </div>
 </template>
 
@@ -17,5 +17,30 @@ export default {
 </script>
 
 <style>
+.counter {
+    background-color: #41B883;
+    border: 2px solid #35495E;
+    color: #fff;
+    text-align: center;
+    padding: 30px 0;
+    margin-bottom: 30px;
+}
 
+.counter-button {
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+    font-size: 1.2rem;
+    font-weight: bold;
+    border: 2px solid #ccc;
+}
+
+.counter-count {
+    margin-top: 0;
+    margin-bottom: 30px;
+    padding: 0;
+    line-height: 1;
+    font-size: 6rem;
+    font-weight: bold;
+}
 </style>
