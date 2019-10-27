e0e7feb8488501d284205bfe445fd2438870a065
Ben Hong <bencodezen@gmail.com>
2019-10-27 09:21:00 -0700

    feature: Add sidebar scrolling

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,55 +1,54 @@
 module.exports = {
-	title: 'VuePress Starter Kit',
-	head: [
-		['link', { rel: 'icon', href: `/favicon.png` }]
-	],
-	ga: '',
-	description: 'This is your page description.',
-	themeConfig: {
-		repo: 'https://github.com/bencodezen/vuepress-starter-kit',
-		editLinks: true,
-		editLinkText: 'Edit this page',
-		lastUpdated: 'Last updated',
-		nav: [
-			{
-				text: 'Home',
-				link: '/'
-			},
-			{
-				text: 'Basic Page',
-				link: '/basic/'
-			},
-			{
-				text: 'Component Example',
-				link: '/component-example'
-			},
-			{
-				text: 'Section',
-				items: [
-					{
-						text: 'Section Introduction',
-						link: '/section/#section-introduction'
-					},
-					{
-						text: 'Some More Content!',
-						link: '/section/#some-more-content'
-					}
-				]
-			},
-			{
-				text: 'Contact',
-				items: [
-					{
-						text: 'Twitter',
-						link: 'https://www.twitter.com/'
-					},
-					{
-						text: 'Email',
-						link: 'mailto:hello@email.com'
-					}
-				]
-			}
-		],
-		docsDir: 'docs'
-	}
+  title: 'VuePress Starter Kit',
+  head: [['link', { rel: 'icon', href: `/favicon.png` }]],
+  ga: '',
+  description: 'This is your page description.',
+  themeConfig: {
+    repo: 'https://github.com/bencodezen/vuepress-starter-kit',
+    editLinks: true,
+    editLinkText: 'Edit this page',
+    lastUpdated: 'Last updated',
+    nav: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Basic Page',
+        link: '/basic/'
+      },
+      {
+        text: 'Component Example',
+        link: '/component-example'
+      },
+      {
+        text: 'Section',
+        items: [
+          {
+            text: 'Section Introduction',
+            link: '/section/#section-introduction'
+          },
+          {
+            text: 'Some More Content!',
+            link: '/section/#some-more-content'
+          }
+        ]
+      },
+      {
+        text: 'Contact',
+        items: [
+          {
+            text: 'Twitter',
+            link: 'https://www.twitter.com/'
+          },
+          {
+            text: 'Email',
+            link: 'mailto:hello@email.com'
+          }
+        ]
+      }
+    ],
+    docsDir: 'docs',
+    plugins: ['@vuepress/active-header-links']
+  }
 }
